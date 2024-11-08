8b40da068fe54cb2e89f199760ce0e2d454643ca
Dmytro Kozin <dimakoua@gmail.com>
2024-11-08 04:49:03 +0000

    remove from git

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Settings:
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/travel_buddy_db")
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
-
-settings = Settings()
--- readme.md
+++
@@ -1,3 +0,0 @@
-source .venv/bin/activate
-uvicorn main:app --reload
-deactivate
