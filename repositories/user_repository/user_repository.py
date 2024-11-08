54199b66fa3c44edb77457248aa6d4471290ad8d
Dmytro Kozin <dimakoua@gmail.com>
2024-11-08 05:06:48 +0000

    update validatinos

--- repositories/user_repository/user_repository.py
+++ repositories/user_repository/user_repository.py
@@ -6,13 +6,6 @@ import re
 class UserRepository:
     @staticmethod
     def save(session: Session, email: str, password: str):
-        if not re.match(r".+@.+\..+", email):
-            raise ValueError("Email is invalid")
-        if len(password) < 8 or not any(char.isdigit() for char in password) \
-                or not any(char.isupper() for char in password) or not any(char.islower() for char in password) \
-                or not re.search(r"[#?!@$%^&*()]", password):
-            raise ValueError("Password does not meet requirements")
-        
         hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
         user = User(email=email, hashed_password=hashed_password)
         session.add(user)
--- schemas/auth/auth.py
+++ schemas/auth/auth.py
@@ -1,11 +1,36 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
+import re
 
 class SignUpPayload(BaseModel):
     class Payload(BaseModel):
         class Data(BaseModel):
-            hashed_password: str
+            password_confirm: str
             password: str
             email: EmailStr
+
+            @field_validator("password")
+            def validate_password(cls, password):
+                # Check password length
+                if len(password) < 8:
+                    raise ValueError("Password must be at least 8 characters long")
+
+                # Check for at least one digit
+                if not any(char.isdigit() for char in password):
+                    raise ValueError("Password must contain at least one digit")
+
+                # Check for at least one uppercase letter
+                if not any(char.isupper() for char in password):
+                    raise ValueError("Password must contain at least one uppercase letter")
+
+                # Check for at least one lowercase letter
+                if not any(char.islower() for char in password):
+                    raise ValueError("Password must contain at least one lowercase letter")
+
+                # Check for at least one special character
+                if not re.search(r"[#?!@$%^&*()]", password):
+                    raise ValueError("Password must contain at least one special character")
+
+                return password
         data: Data
 
     payload: Payload
