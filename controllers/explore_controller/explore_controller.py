026834bfb3df3389ed8e8cefdd06368eda075796
Dmytro Kozin <dimakoua@gmail.com>
2024-11-09 18:42:33 +0000

    add new controllers

---
+++ controllers/explore_controller/explore_controller.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from schemas.sduf_request.sduf_request import SdufRequest
+from repositories.user_repository.user_repository import UserRepository
+from database import get_db
+
+router = APIRouter()
+
+@router.post("/")
+async def profile(params: SdufRequest, db: Session = Depends(get_db)):
+    return {"message": "Explore preview screen"}
+
+@router.post("/load-more")
+async def profile_edit(params: SdufRequest, db: Session = Depends(get_db)):
+     return {"message": "Load more api"}
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from database import Base, engine
 from controllers.auth_controller import auth_controller
+from controllers.profile_controller import profile_controller
+from controllers.explore_controller import explore_controller
+from controllers.chat_controller import chat_controller
 
 app = FastAPI()
 
@@ -9,3 +12,6 @@ Base.metadata.create_all(bind=engine)
 
 # Include routers
 app.include_router(auth_controller.router, prefix="/auth", tags=["auth"])
+app.include_router(profile_controller.router, prefix="/profile", tags=["profile"])
+app.include_router(chat_controller.router, prefix="/chat", tags=["chat"])
+app.include_router(explore_controller.router, prefix="/explore", tags=["explore"])
