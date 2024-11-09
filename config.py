a623b9ab46d436c3e17b4e87a84c0fffd0baa1ea
Dmytro Kozin <dimakoua@gmail.com>
2024-11-09 19:37:20 +0000

    add test db url

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/travel_buddy_db")
+    TEST_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/test_travel_buddy_db")
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 
 settings = Settings()
