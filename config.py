f3bfe0ce918f09182d13b6ddb36c2cb9b0245671
Dmytro Kozin <dimakoua@gmail.com>
2024-11-10 04:46:58 +0000

    update sduf api client

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/travel_buddy_db")
     TEST_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/test_travel_buddy_db")
     SDUF_BASE_URL = os.getenv("SDUF_BASE_URL", "https://sduf.net")
+    SDUF_PROJECT_TOKEN = os.getenv("SDUF_PROJECT_TOKEN", "N4DBMiKlJXbpStsZrHwHyX1nkxw9RAvurP3GgwnHuOc=")
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 
 settings = Settings()
--- sduf/api_client.py
+++ sduf/api_client.py
@@ -2,8 +2,13 @@ from config import settings
 import requests
 
 BASE_URL = settings.SDUF_BASE_URL
+PROJECT_TOKEN = settings.SDUF_PROJECT_TOKEN
 
 def send_event(body):
-    response = requests.post(f"{BASE_URL}/push/event", body)
+    headers = {
+        "Content-Type": "application/json",
+        "x-project-token": PROJECT_TOKEN
+    }
+    response = requests.post(f"{BASE_URL}/api/v1/push/event", json=body, headers=headers)
     response.raise_for_status()
-    return response.json()
+    return response
