0b79462565029c5fc9859cbf50d4721d64484dae
Dmytro Kozin <dimakoua@gmail.com>
2024-11-21 03:46:49 +0000

    update base component

--- components/base_component.py
+++ components/base_component.py
@@ -1,6 +1,12 @@
 import json
+import uuid
 
 class BaseComponent():
+    def __init__(self):
+        self.id = str(uuid.uuid4()),
+        self.name = None,
+        self.data = {}
+
     def to_json(self) -> str:
         """
         Convert the instance to a JSON string.
