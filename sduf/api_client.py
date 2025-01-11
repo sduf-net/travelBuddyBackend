0c90d4ea7f20de4399b3fe61f4ee0207a53e9e84
Dmytro Kozin <dimakoua@gmail.com>
2025-01-11 02:42:26 +0000

    update SDUF project token to API token in configuration and API client

--- sduf/api_client.py
+++ sduf/api_client.py
@@ -3,12 +3,12 @@ import requests
 from schemas.sduf_request.sduf_request import SdufEvent
 
 BASE_URL = get_settings().SDUF_BASE_URL
-PROJECT_TOKEN = get_settings().SDUF_PROJECT_TOKEN
+PROJECT_TOKEN = get_settings().SDUF_PROJECT_API_TOKEN
 
 def send_event(body: SdufEvent):
     headers = {
         "Content-Type": "application/json",
-        "x-project-token": PROJECT_TOKEN
+        "x-api-token": PROJECT_TOKEN
     }
     response = requests.post(f"{BASE_URL}/api/v1/push/event", json=body.model_dump(), headers=headers)
     response.raise_for_status()
